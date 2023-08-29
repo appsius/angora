445669abff4f83eb3f48b087f6749da41477914b
ilyas ustunkaya <ilyas.ustunkaya@ithinka.com>
2023-08-29 15:51:03 +0300

    Add basic onboarding setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata = {
   title: "Angora",
   description: "An app where lost people find each other!",
 };
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
+import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
-import "../globals.css";
 import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
