af4390c02fc03788d1b3c37a240cd1e62c557003
ilyas ustunkaya <ilyas.ustunkaya@ithinka.com>
2023-08-29 11:01:49 +0300

    Detact active button and style it"

--- app/components/shared/LeftSidebar.tsx
+++ app/components/shared/LeftSidebar.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter, usePathname } from "next/navigation";
 
 function LeftSidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname == link.route;
+
           return (
-            <div>
+            <div key={link.label}>
               <Link
                 href={link.route}
-                key={link.label}
-                className="leftsidebar_link"
+                className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
               >
                 <Image
                   src={link.imgURL}
