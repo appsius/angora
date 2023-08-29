1a87eb1dcaae26e8e4f6e492fa6726063dc55dc6
ilyas ustunkaya <ilyas.ustunkaya@ithinka.com>
2023-08-29 11:11:24 +0300

    Integrate logout into leftsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -4,6 +4,7 @@ import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter, usePathname } from "next/navigation";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
 
 function LeftSidebar() {
   const router = useRouter();
@@ -35,6 +36,22 @@ function LeftSidebar() {
             </div>
           );
         })}
+      </div>
+
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src={"./assets/logout.svg"}
+                alt={"logout"}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   );
