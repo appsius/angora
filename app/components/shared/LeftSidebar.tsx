6881a3c1a43a4d532ecd5df7231668ccb55a2ca0
ilyas ustunkaya <ilyas.ustunkaya@ithinka.com>
2023-08-29 10:51:49 +0300

    Add initial leftsidebar design

--- app/components/shared/LeftSidebar.tsx
+++ app/components/shared/LeftSidebar.tsx
@@ -1,5 +1,34 @@
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+
 function LeftSidebar() {
-  return <h1>LeftSidebar</h1>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          return (
+            <div>
+              <Link
+                href={link.route}
+                key={link.label}
+                className="leftsidebar_link"
+              >
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  width={24}
+                  height={24}
+                  style={{ height: "auto" }}
+                />
+                <p className="text-light-1 max-lg:hidden">{link.label}</p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default LeftSidebar;
