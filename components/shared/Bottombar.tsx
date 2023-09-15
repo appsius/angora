c68f6b2bf28d7585b66aa0398b6015923f5abea1
appsius <ilyas.ustunkaya234434@gmail.com>
2023-09-15 11:52:07 +0300

    Add commenting functionality

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,17 +1,17 @@
-"use client";
+'use client';
 
-import { sidebarLinks } from "@/constants";
-import Link from "next/link";
-import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
+import { sidebarLinks } from '@/constants';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
 
 function Bottombar() {
   const router = useRouter();
   const pathname = usePathname();
 
   return (
-    <section className="bottombar">
-      <div className="bottombar_container">
+    <section className='bottombar'>
+      <div className='bottombar_container'>
         {sidebarLinks.map((link) => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
@@ -21,16 +21,16 @@ function Bottombar() {
             <div key={link.label}>
               <Link
                 href={link.route}
-                className={`bottombar_link ${isActive && "bg-primary-500"}`}
+                className={`bottombar_link ${isActive && 'bg-primary-500'}`}
               >
                 <Image
                   src={link.imgURL}
                   alt={link.label}
                   width={24}
                   height={24}
-                  style={{ height: "auto" }}
+                  style={{ height: 'auto' }}
                 />
-                <p className="text-subtle-medium text-light-1 max-xs:hidden">
+                <p className='text-subtle-medium text-light-1 max-xs:hidden'>
                   {link.label.split(/\s+/)[0]}
                 </p>
               </Link>
