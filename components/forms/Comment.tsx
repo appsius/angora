50d340d040f7cd193fee1a58ba0e61561a420a26
appsius <ilyas.ustunkaya234434@gmail.com>
2023-09-18 09:02:36 +0300

    Fix deployment Promise Element bug

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -47,40 +47,42 @@ const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='comment-form'>
-        <FormField
-          control={form.control}
-          name='thread'
-          render={({ field }) => (
-            <FormItem className='flex items-center w-full gap-3'>
-              <FormLabel>
-                <Image
-                  src={currentUserImg}
-                  alt='Profile image'
-                  width={48}
-                  height={48}
-                  className='rounded-full object-cover'
-                  sizes='100vw'
-                />
-              </FormLabel>
-              <FormControl className='border-none bg-transparent'>
-                <Input
-                  type='text'
-                  placeholder='Comment...'
-                  className='text-light-1 no-focus outline-none'
-                  {...field}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='comment-form'>
+          <FormField
+            control={form.control}
+            name='thread'
+            render={({ field }) => (
+              <FormItem className='flex items-center w-full gap-3'>
+                <FormLabel>
+                  <Image
+                    src={currentUserImg}
+                    alt='Profile image'
+                    width={48}
+                    height={48}
+                    className='rounded-full object-cover'
+                    sizes='100vw'
+                  />
+                </FormLabel>
+                <FormControl className='border-none bg-transparent'>
+                  <Input
+                    type='text'
+                    placeholder='Comment...'
+                    className='text-light-1 no-focus outline-none'
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-        <Button type='submit' className='comment-form_btn'>
-          Reply
-        </Button>
-      </form>
-    </Form>
+          <Button type='submit' className='comment-form_btn'>
+            Reply
+          </Button>
+        </form>
+      </Form>
+    </>
   );
 };
 
--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -14,29 +14,31 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
   if (!result) redirect('/');
 
   return (
-    <section className='flex flex-col gap-10 mt-9'>
-      {result.threads.map((thread: any) => (
-        <ThreadCard
-          key={thread._id}
-          id={thread._id}
-          currentUserId={currentUserId}
-          parentId={thread.parentId}
-          content={thread.text}
-          author={
-            accountType === 'User'
-              ? { name: result.name, image: result.image, id: result.id }
-              : {
-                  name: thread.author.name,
-                  image: thread.author.image,
-                  id: thread.author.id,
-                }
-          }
-          community={thread.community} // todo
-          createdAt={thread.createdAt}
-          comments={thread.children}
-        />
-      ))}
-    </section>
+    <>
+      <section className='flex flex-col gap-10 mt-9'>
+        {result.threads.map((thread: any) => (
+          <ThreadCard
+            key={thread._id}
+            id={thread._id}
+            currentUserId={currentUserId}
+            parentId={thread.parentId}
+            content={thread.text}
+            author={
+              accountType === 'User'
+                ? { name: result.name, image: result.image, id: result.id }
+                : {
+                    name: thread.author.name,
+                    image: thread.author.image,
+                    id: thread.author.id,
+                  }
+            }
+            community={thread.community} // todo
+            createdAt={thread.createdAt}
+            comments={thread.children}
+          />
+        ))}
+      </section>
+    </>
   );
 };
 
